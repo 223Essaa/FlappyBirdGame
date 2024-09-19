e12b344e0c4c8c7d5f088dabe0b0def1f372996b
223Essaa <essa.ongchangco223@gmail.com>
2024-09-19 23:42:43 +1200

    first commit

---
+++ Classlib.py
@@ -0,0 +1,117 @@
+import pygame
+# from Flappybird import Flying
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self,x,y, Flying):
+      super().__init__() #Inherit functionalities
+      self.images = []
+      self.index = 0
+      self.counter = 0
+      
+      #Goes through the images to animate the bird
+      for num in range(1,4):
+         img = pygame.image.load(f"img/bird{num}.png")
+         self.images.append(img)
+
+      self.image = self.images[self.index]
+      self.rect = self.image.get_rect()
+      self.rect.center = [x,y]
+      self.clicked = False
+     
+      
+      
+      #Gravity
+      self.vel = 0 
+
+      #main
+      self.Flying = Flying
+      
+    def update(self):
+      #Constantly changes the players y value. Gravity
+      if self.Flying == True:
+        self.vel += 0.5
+        if self.vel > 8:
+          self.vel = 8
+        #Prevents the bird from falling through the window 
+        if self.rect.bottom < 768:
+          self.rect.y += int(self.vel)
+
+      #Flapping
+      if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False: 
+          self.clicked = True
+          self.vel = -10
+      if pygame.mouse.get_pressed()[0] == 0:
+          self.clicked = False
+      
+
+      #handling the animations
+      self.counter += 1
+      flap_cooldown = 5
+      #checks the animation counts to know when to reset
+      if self.counter > flap_cooldown:
+        self.counter = 0
+        self.index += 1
+        if self.index >= len(self.images):
+            self.index = 0
+      self.image = self.images[self.index]
+     
+
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, position, open, scroll_speed):
+      super().__init__()
+      self.image = pygame.image.load('img/pipe.png')
+      self.rect = self.image.get_rect()
+      self.open = False
+      
+
+      if position == 1 and open == False: 
+        self.image = pygame.transform.flip(self.image, False, True)
+        self.rect.bottomleft = [x,y]
+         
+      if position == -1 and open == False:
+         self.rect.topleft = [x, y]
+
+      #Flappybird.py imports
+      self.scroll_speed = scroll_speed  
+
+    def update(self):
+      self.rect.x -= self.scroll_speed
+      if self.rect.right < 0:
+         self.kill()
+         
+
+    def move(self, change_x, change_y):
+       self.rect.x = change_x
+       self.rect.y = change_y
+      
+			    
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, SCREEN_WIDTH):
+      super().__init__()
+
+      self.image = pygame.image.load("img/bullet.png")
+      self.rect = self.image.get_rect()
+      self.rect.center = [x,y]
+      self.vel = 7
+
+      #Main imports
+      self.SCREEN_WIDTH = SCREEN_WIDTH
+      
+    def update(self):
+       self.rect.x += self.vel
+       if self.rect.x > self.SCREEN_WIDTH:
+          self.kill()
+
+class Target(pygame.sprite.Sprite):
+    def __init__(self, x, y, scroll_speed):
+       super().__init__()
+       self.image = pygame.image.load("img/Target.png")
+       self.rect = self.image.get_rect()
+       self.rect.center = [x,y]
+
+       #Flappybird.py imports
+       self.scroll_speed = scroll_speed
+    def update(self):
+       self.rect.x -= self.scroll_speed
+    
---
+++ Flappybird.py
@@ -0,0 +1,175 @@
+import pygame
+import os
+import sys
+from pygame.locals import *
+import random
+from Classlib import *
+
+pygame.init()
+
+#Classes
+
+
+
+
+clock = pygame.time.Clock()
+FPS = 60
+
+SCREEN_WIDTH = 864
+SCREEN_HEIGHT = 937
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption('Flappy Bird Game')
+bg_image = pygame.image.load("img/bg.png")
+ground = pygame.image.load("img/ground.png")
+
+
+#color
+WHITE = (255,255,255)
+#Game variables
+pipe_gap = 150
+pipe_frequency = 4000 #in milliseconds
+last_pipe = pygame.time.get_ticks() 
+#Variables for making the ground animation
+ground_scroll = 0
+scroll_speed  = 5
+#Game Checkers
+game_over = pass_pipe = button_hit = Flying = False
+score = 0
+bullet = []
+
+font = pygame.font.SysFont('Bauhaus 93', 60)
+   
+#function for outputting text onto the screen
+def draw_text(text, font, text_col, x, y):
+	img = font.render(text, True, text_col)
+	screen.blit(img, (x, y))
+
+
+
+
+#Making a pygame sprite list 
+bullet_group, bird_group, target_group, pipe_group = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
+bird = Bird(100, int(SCREEN_HEIGHT/2), Flying)
+bird_group.add(bird)
+
+
+running = True
+shoot = True
+while running:
+    clock.tick(FPS)
+    
+    
+    print(button_hit)
+    
+    #Variable storing keyboard inputs
+    keys = pygame.key.get_pressed()
+
+    #Getting the birds coordinates 
+    for sprites in bird_group:
+      bird_X,bird_y = sprites.rect.center
+      sprites.Flying = Flying
+    
+
+    
+
+    #Shooting the bullets
+    if keys[K_SPACE] == 1 and shoot == True:
+        shoot = False
+        tempbullet = Bullet(bird_X,bird_y, SCREEN_WIDTH)
+        bullet_group.add(tempbullet)
+    if keys[K_SPACE] == 0 and shoot == False:
+       shoot = True
+
+    #Drawing and updating stuff on the screen
+    screen.blit(bg_image,(0,0))
+
+    #Drawing the bullet
+    bullet_group.draw(screen)
+    bullet_group.update()
+
+    #Drawing the target
+    target_group.draw(screen)
+
+    #Drawing the pipe
+    pipe_group.draw(screen)
+
+    #Drawing the player
+    bird_group.draw(screen)
+    bird_group.update()
+
+    #Draws and scrolls the background
+    screen.blit(ground,(ground_scroll,768))
+
+    #checks if the bird went past a pipe, increasing the score.
+    if len(pipe_group) > 0:
+      if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left\
+        and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right\
+        and pass_pipe == False:
+        pass_pipe = True
+      if pass_pipe == True:
+        if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
+          score += 1
+          scroll_speed += 1
+          pass_pipe = False
+    draw_text(str(score), font, WHITE, int(SCREEN_WIDTH / 2), 50)
+    
+    #Event handeling
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+      if event.type == pygame.MOUSEBUTTONDOWN and Flying == False and game_over == False:
+        Flying = True
+    
+  
+    #-------------Game Logic (Update game state here)-------------
+    if game_over == False and Flying == True:
+      time_now = pygame.time.get_ticks() 
+      if time_now - last_pipe > pipe_frequency:
+        #Making the pipes
+        pipe_height = random.randint(-100, 100)
+        btm_pipe = Pipe(SCREEN_WIDTH, int(SCREEN_HEIGHT/2) + pipe_height, 1, False, scroll_speed)
+        top_pipe = Pipe(SCREEN_WIDTH, int(SCREEN_HEIGHT/2) + pipe_height, -1, False, scroll_speed)
+        
+        target_height = random.randint(0, 100)
+        pipe_target = Target(SCREEN_WIDTH - 50, int(SCREEN_HEIGHT/2) + target_height, scroll_speed)
+
+        target_group.add(pipe_target)
+        pipe_group.add(btm_pipe)
+        pipe_group.add(top_pipe)
+        last_pipe = time_now
+      
+
+      pipe_group.update()
+      target_group.update()
+    
+
+      #Resets ground position to make the ground look like it is moving
+      if abs(ground_scroll) > 35:
+          ground_scroll = 0
+      
+      #Detects if the bullet hits the target to open the pipe up.
+      if pygame.sprite.groupcollide(bullet_group, target_group, True, True):
+        btm_pipe.move(btm_pipe.rect.x, btm_pipe.rect.y - (pipe_gap/2))
+        top_pipe.move(top_pipe.rect.x, top_pipe.rect.y + (pipe_gap/2))
+
+      if pygame.sprite.groupcollide(bird_group, pipe_group, False, False):
+        game_over = True
+        Flying = False
+
+      if bird.rect.bottom > 768:
+        game_over = True
+        Flying = False
+      
+      #Drawing the stuff onto the screen
+      ground_scroll -= scroll_speed  #Varaible to make the ground move
+      
+    
+
+    pygame.display.update()
+    
+    
+
+
+
